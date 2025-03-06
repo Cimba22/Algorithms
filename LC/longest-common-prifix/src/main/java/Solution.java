64db7c5a9fcf54284727b0e84d4943bbf2de70ac
Oleksandra Mala <oleksandra.s.mala@gmail.com>
2025-03-06 21:03:35 +0100

    Update of 14. Longest Common Prefix in LC.

--- LC/longest-common-prifix/src/main/java/Solution.java
+++ LC/longest-common-prifix/src/main/java/Solution.java
@@ -2,15 +2,15 @@
 public class Solution {
     public static String longestCommonPrefix(String[] strs) {
         String ans = strs[0];
-        for (int i = 0; i < strs.length; i++) {
-            while (strs[i].indexOf(ans) != 0)
-                    ans = ans.substring(0, ans.length() - 1);
+        for (String str : strs) {
+            while (str.indexOf(ans) != 0)
+                ans = ans.substring(0, ans.length() - 1);
         }
         return ans;
     }
 
     public static void main(String[] args) {
-        String[] strs = {"flower","flow","flight"};
+        String[] strs = {"flower", "flow", "flight"};
         System.out.println(longestCommonPrefix(strs));
     }
 }
