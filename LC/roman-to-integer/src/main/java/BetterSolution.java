cfbb7e0134cab4d4aab3e8049bc5bb50c361795d
Oleksandra Mala <oleksandra.s.mala@gmail.com>
2025-03-06 20:47:21 +0100

    Update 13. Roman to Integer in LC.

--- LC/roman-to-integer/src/main/java/BetterSolution.java
+++ LC/roman-to-integer/src/main/java/BetterSolution.java
@@ -1,20 +1,21 @@
 //Runtime of this solution is 3ms
 public class BetterSolution {
     public int romanToInt(String s) {
-            int ans = 0, num = 0;
-            for (int i = s.length()-1; i >= 0; i--) {
-                switch(s.charAt(i)) {
-                    case 'I': num = 1; break;
-                    case 'V': num = 5; break;
-                    case 'X': num = 10; break;
-                    case 'L': num = 50; break;
-                    case 'C': num = 100; break;
-                    case 'D': num = 500; break;
-                    case 'M': num = 1000; break;
-                }
-                if (4 * num < ans) ans -= num;
-                else ans += num;
-            }
-            return ans;
+        int ans = 0, num = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            num = switch (s.charAt(i)) {
+                case 'I' -> 1;
+                case 'V' -> 5;
+                case 'X' -> 10;
+                case 'L' -> 50;
+                case 'C' -> 100;
+                case 'D' -> 500;
+                case 'M' -> 1000;
+                default -> num;
+            };
+            if (4 * num < ans) ans -= num;
+            else ans += num;
         }
+        return ans;
+    }
 }
