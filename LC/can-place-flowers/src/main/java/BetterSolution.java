f1f60c3bbbeec615d4936076111f307761e46d7d
Oleksandra Mala <oleksandra.s.mala@gmail.com>
2025-03-06 22:00:46 +0100

    Update of 605. Can Place Flowers in LC.

--- LC/can-place-flowers/src/main/java/BetterSolution.java
+++ LC/can-place-flowers/src/main/java/BetterSolution.java
@@ -1,17 +1,21 @@
 public class BetterSolution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        for (int i = 0; i < flowerbed.length; i = i + 2) {
 
-            if (flowerbed[i] == 0) {
-                if (i == flowerbed.length - 1 || flowerbed[i] == flowerbed[i + 1]) {
-                    n--;
-                } else {
-                    i++;
-                }
+        int i = 0;
+        while (i < flowerbed.length && n > 0) {
+            if (flowerbed[i] == 1) {
+                i += 2;
+            } else if ((i == 0 || flowerbed[i - 1] == 0)
+                    && (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
+                flowerbed[i] = 1;
+                n--;
+                i += 2;
+            } else {
+                i++;
             }
-
-
         }
-        return n <= 0;
+
+        return n == 0;
+
     }
 }
